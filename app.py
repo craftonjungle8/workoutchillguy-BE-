f7880ff1a7c95366e14abfa83e458303c9a55ef5
Kim JunHyeok <94499659+hmmwtf@users.noreply.github.com>
2025-03-11 10:00:07 +0900

    [ADD] folder

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def main():
    return 'This is Home!'
 
+@app.route('/login')
+def login():
+   return 'This is Login'
+
+@app.route('/signup')
+def signUp():
+   return 'This is Signup'
+
+@app.route('/mate')
+def findMate():
+   return 'This is Findmate'
+
+@app.route('/diary')
+def diary():
+   return 'This is Diary'
+
+@app.route('/board')
+def board():
+   return 'This is borad'
 if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
